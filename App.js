66337e7f7f04639d6db52f88e7d7d54f5ad5da9d
ae-yengb3 <yengbeamanda@gmail.com>
2023-06-28 14:43:13 +0000

    theme

--- App.js
+++ App.js
@@ -4,17 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View><Text> Hello World!</Text></View>
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  backgroundColor: 'red'
   },
 });
