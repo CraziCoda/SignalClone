c03112e178039dcec959a1ae8d838fdb51e3e7c4
chayira03 <chashiraventures@gmail.com>
2023-07-12 14:10:37 +0100

    Help screen

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Help from './screens/settings/Help';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Help/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles=StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex : 1,
+    marginTop: 10
+  }
+})
---
+++ screens/settings/Help.js
@@ -0,0 +1,129 @@
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native"
+export default function Help(){
+    return(<View style={styles.container}>
+    
+        <View style={styles.header}>
+            <Text>X</Text>
+            <Text>Help</Text>
+        </View>
+        <View>
+            <TouchableHighlight activeOpacity={0.6} underlayColor="lightgrey" onPress={()=>{}}>
+            <View style={styles.label}>
+                <Text>Support Center</Text>
+                <Text>B</Text>
+            </View>
+          
+            </TouchableHighlight>
+        <TouchableHighlight activeOpacity={0.6} underlayColor="lightgrey" onPress={()=>{}}>
+        <View style={styles.label2}>
+                <Text>Contact Us</Text>
+            </View>
+        </TouchableHighlight>
+        <View style={styles.label3}>
+                <Text>Version</Text>
+                <Text>6.21.3</Text>
+            </View>
+        <TouchableHighlight activeOpacity={0.6} underlayColor="lightgrey" onPress={()=>{}}>
+        <View style={styles.label4}>
+                <Text>Debug log</Text>
+            </View>
+        </TouchableHighlight>
+        <TouchableHighlight activeOpacity={0.6} underlayColor="lightgrey" onPress={()=>{}}>
+        <View style={styles.label5}>
+                <Text>Terms & Privacy Policy</Text>
+                <Text>X</Text>
+            </View>
+        </TouchableHighlight>
+        <View style={styles.label6}>
+                <Text>Copyright Signal Messenger</Text>
+                <Text>Licensed under the GNU AGPLv3</Text>
+            </View>
+        </View>
+       
+    </View>)
+
+}
+
+const styles=StyleSheet.create({
+
+   container:{
+
+   } ,
+
+   header:{
+    height: '25%',
+    width: '100%',
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 1,
+    paddingRight: '70%',
+    paddingBottom: 10,
+    paddingTop: 1,
+    justifyContent: "space-around"
+   } ,
+
+   label: {
+    width: '100%',
+    height: 50,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 30,
+    marginTop: -30,
+    paddingRight: 30,
+    justifyContent: 'space-between'
+    
+   } ,
+   label2: {
+    marginTop: 1,
+    width: '100%',
+    height: 50,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 30
+   } ,
+   label3: {
+    marginTop: 50,
+    width: '100%',
+    height: 50,
+    alignContent: "center",
+    marginRight: '90%',
+    flexDirection: "column",
+    paddingLeft: 30
+
+   } ,
+   label4: {
+    marginTop: 1,
+    width: '100%',
+    height: 50,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 30
+
+   } ,
+   label5: {
+    marginTop: 20,
+    width: '100%',
+    height: 50,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 30,
+    paddingRight: 30,
+    justifyContent: "space-between"
+
+   } ,
+   label6: {
+    marginTop: 20,
+    width: '100%',
+    height: 50,
+    alignContent: "center",
+    marginRight: '90%',
+    flexDirection: "column",
+    paddingLeft: 30
+
+   } 
+   
+   
+
+
+   
+})
